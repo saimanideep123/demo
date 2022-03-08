f8588ed06dd57fd5b09ff7bf43dd1d8453005583
saimanideep123 <94214378+saimanideep123@users.noreply.github.com>
2022-03-08 13:32:25 +0530

    Add files via upload
    created a department demo project

---
+++ Department.java
@@ -0,0 +1,62 @@
+package com.spring.first.app.backend.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.AUTO)
+	private Long departmentId;
+	private String departmentName;
+	private String departmentAddress;
+	private String departmentCode;
+	
+	
+	public Department(Long departmentId, String departmentName, String departmentAddress, String depatmentCode) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.departmentAddress = departmentAddress;
+		this.departmentCode = depatmentCode;
+	}
+	public Department() {
+		
+	}
+	
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public String getDepartmentAddress() {
+		return departmentAddress;
+	}
+	public void setDepartmentAddress(String departmentAddress) {
+		this.departmentAddress = departmentAddress;
+	}
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+	public void setdepartmentCode(String depatmentCode) {
+		this.departmentCode = depatmentCode;
+	}
+	@Override
+	public String toString() {
+		return "Department [departmentId=" + departmentId + ", departmentName=" + departmentName
+				+ ", departmentAddress=" + departmentAddress + ", depatmentCode=" + departmentCode + "]";
+	}
+	
+	
+}
---
+++ DepartmentRepository.java
@@ -0,0 +1,17 @@
+package com.spring.first.app.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.first.app.backend.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+	
+	public Department findByDepartmentName(String departmentName);
+	
+	public Department findByDepartmentNameIgnorecase(String departmentName);
+	
+	
+
+}
